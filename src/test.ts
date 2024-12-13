f246d99d41f852f2724c43c93e3d9909e52a2297
Chris Rickman <crickman@skillpulse.ai>
2024-12-12 16:32:29 -0800

    Add SHA512 to tests

--- src/test.ts
+++ src/test.ts
@@ -3,19 +3,25 @@ import {
   generateUri,
   generateToken,
   validateToken,
+  hashAlgorithm,
 } from "./index"
 
 const referenceLabel = "Superbacked"
 const referenceUsername = "john@protonmail.com"
 const referenceSecret = "DMJKP7AU22WKWRG3DNIQ3ERA"
 const referenceIssuer = referenceLabel
-const referenceUri =
-  "otpauth://totp/Superbacked:john%40protonmail.com?secret=DMJKP7AU22WKWRG3DNIQ3ERA&issuer=Superbacked&algorithm=SHA1&digits=6&period=30"
-const referenceUri256 =
-  "otpauth://totp/Superbacked:john%40protonmail.com?secret=DMJKP7AU22WKWRG3DNIQ3ERA&issuer=Superbacked&algorithm=SHA256&digits=6&period=30"
+const referenceUri = {
+  SHA1: "otpauth://totp/Superbacked:john%40protonmail.com?secret=DMJKP7AU22WKWRG3DNIQ3ERA&issuer=Superbacked&algorithm=SHA1&digits=6&period=30",
+  SHA256: "otpauth://totp/Superbacked:john%40protonmail.com?secret=DMJKP7AU22WKWRG3DNIQ3ERA&issuer=Superbacked&algorithm=SHA256&digits=6&period=30",
+  SHA512: "otpauth://totp/Superbacked:john%40protonmail.com?secret=DMJKP7AU22WKWRG3DNIQ3ERA&issuer=Superbacked&algorithm=SHA512&digits=6&period=30",
+};
 const referenceTimestamps = [1664596800000, 1664596770000]
-const referenceTokens = ["616692", "415925"]
-const referenceTokens256 = ["067612", "654664"]
+const referenceTokens = {
+  SHA1: ["616692", "415925"],
+  SHA256: ["067612", "654664"],
+  SHA512: ["431432", "415901"],
+}
+const referenceDimensions: (undefined | hashAlgorithm)[] = [undefined, "SHA1", "SHA256", "SHA512"];
 
 test("generate secret", async () => {
   const secret = generateSecret()
@@ -28,44 +34,30 @@ test("generate secret using user-defined length", async () => {
 })
 
 test("generate URI", async () => {
-  const uri = generateUri(
-    referenceLabel,
-    referenceUsername,
-    referenceSecret,
-    referenceIssuer
-  )
-  expect(uri).toEqual(referenceUri)
-})
-
-test("generate URI using SHA256", async () => {
-  const uri = generateUri(
-    referenceLabel,
-    referenceUsername,
-    referenceSecret,
-    referenceIssuer,
-    'SHA256'
-  )
-  expect(uri).toEqual(referenceUri256)
+  for (const algorithm of referenceDimensions) {
+    const uri = generateUri(
+      referenceLabel,
+      referenceUsername,
+      referenceSecret,
+      referenceIssuer,
+      algorithm
+    )
+    expect(uri).toEqual(referenceUri[algorithm ?? 'SHA1'])
+  }
 })
 
 test("generate token", async () => {
-  const token = generateToken(referenceSecret)
-  expect(token).toMatch(/[0-9]{6}/)
-})
-
-test("generate token using SHA256", async () => {
-  const token = generateToken(referenceSecret, 'SHA256', undefined)
-  expect(token).toMatch(/[0-9]{6}/)
+  for (const algorithm of referenceDimensions) {
+    const token = generateToken(referenceSecret, algorithm)
+    expect(token).toMatch(/[0-9]{6}/)
+  }
 })
 
 test("generate token using reference timestamp", async () => {
-  const token = generateToken(referenceSecret, undefined, referenceTimestamps[0])
-  expect(token).toEqual(referenceTokens[0])
-})
-
-test("generate token using reference timestamp and SHA256", async () => {
-  const token = generateToken(referenceSecret, 'SHA256', referenceTimestamps[0])
-  expect(token).toEqual(referenceTokens256[0])
+  for (const algorithm of referenceDimensions) {
+    const token = generateToken(referenceSecret, algorithm, referenceTimestamps[0])
+    expect(token).toEqual(referenceTokens[algorithm ?? 'SHA1'][0])
+  }
 })
 
 test("validate invalid token", async () => {
@@ -74,89 +66,66 @@ test("validate invalid token", async () => {
 })
 
 test("validate valid token", async () => {
-  const result = validateToken(
-    referenceSecret,
-    referenceTokens[0],
-    1,
-    undefined,
-    referenceTimestamps[0]
-  )
-  expect(result).toEqual(true)
-})
-
-test("validate valid token using SHA256", async () => {
-  const result = validateToken(
-    referenceSecret,
-    referenceTokens256[0],
-    1,
-    'SHA256',
-    referenceTimestamps[0]
-  )
-  expect(result).toEqual(true)
+  for (const algorithm of referenceDimensions) {
+    const result = validateToken(
+      referenceSecret,
+      referenceTokens[algorithm ?? 'SHA1'][0],
+      1,
+      algorithm,
+      referenceTimestamps[0]
+    )
+    expect(result).toEqual(true)
+  }
 })
 
 test("validate valid token using lower case secret", async () => {
-  const result = validateToken(
-    referenceSecret.toLowerCase(),
-    referenceTokens[0],
-    1,
-    undefined,
-    referenceTimestamps[0]
-  )
-  expect(result).toEqual(true)
+  for (const algorithm of referenceDimensions) {
+    const result = validateToken(
+      referenceSecret.toLowerCase(),
+      referenceTokens[algorithm ?? 'SHA1'][0],
+      1,
+      algorithm,
+      referenceTimestamps[0]
+    )
+    expect(result).toEqual(true)
+  }
 })
 
 test("validate valid but expired past token", async () => {
-  const result = validateToken(
-    referenceSecret,
-    referenceTokens[1],
-    1,
-    undefined,
-    referenceTimestamps[0]
-  )
-  expect(result).toEqual(false)
+  for (const algorithm of referenceDimensions) {
+    const result = validateToken(
+      referenceSecret,
+      referenceTokens[algorithm ?? 'SHA1'][1],
+      1,
+      algorithm,
+      referenceTimestamps[0]
+    )
+    expect(result).toEqual(false)
+  }
 })
 
 test("validate valid past token", async () => {
-  const result = validateToken(
-    referenceSecret,
-    referenceTokens[1],
-    1,
-    undefined,
-    referenceTimestamps[1]
-  )
-  expect(result).toEqual(true)
-})
-
-test("validate valid past token using SHA256", async () => {
-  const result = validateToken(
-    referenceSecret,
-    referenceTokens256[1],
-    1,
-    'SHA256',
-    referenceTimestamps[1]
-  )
-  expect(result).toEqual(true)
+  for (const algorithm of referenceDimensions) {
+    const result = validateToken(
+      referenceSecret,
+      referenceTokens[algorithm ?? 'SHA1'][1],
+      1,
+      algorithm,
+      referenceTimestamps[1]
+    )
+    expect(result).toEqual(true)
+  }
 })
 
 test("validate valid past token using threshold 2", async () => {
-  const result = validateToken(
-    referenceSecret,
-    referenceTokens[1],
-    2,
-    undefined,
-    referenceTimestamps[0]
-  )
-  expect(result).toEqual(true)
-})
-
-test("validate valid past token using threshold 2 and SHA256", async () => {
-  const result = validateToken(
-    referenceSecret,
-    referenceTokens256[1],
-    2,
-    'SHA256',
-    referenceTimestamps[0]
-  )
-  expect(result).toEqual(true)
+  for (const algorithm of referenceDimensions) {
+    const result = validateToken(
+      referenceSecret,
+      referenceTokens[algorithm ?? 'SHA1'][1],
+      2,
+      algorithm,
+      referenceTimestamps[0]
+    )
+    expect(result).toEqual(true)
+  }
 })
